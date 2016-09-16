c733890e1edbcd0891402757e937d829a020128a
markreo <giap.nguyenvan.562@gmail.com>
2016-09-17 03:06:25 +0700

    next upload

---
+++ test/unit/com/quanlinhatro/auth/UserRoleControllerSpec.groovy
@@ -0,0 +1,20 @@
+package com.quanlinhatro.auth
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(UserRoleController)
+class UserRoleControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
